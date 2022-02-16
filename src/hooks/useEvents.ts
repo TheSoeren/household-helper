e9a63cb7005552d605c599b5ee6192bae6116809
Søren <imhofabian@gmail.com>
2022-02-16 22:26:05 +0100

    test pre-commit hook #4

--- src/hooks/useEvents.ts
+++ src/hooks/useEvents.ts
@@ -1,19 +1,19 @@
-import Event from "@/models/Event";
-import { dbEventsToEvents } from "@/utils/dataConverter";
-import useSWR from "swr";
-import { getRequest } from "@/utils/httpRequests";
+import Event from '@/models/Event'
+import { dbEventsToEvents } from '@/utils/dataConverter'
+import useSWR from 'swr'
+import { getRequest } from '@/utils/httpRequests'
 
 export default function useEvents(initialValues?: Event[]) {
-  const fetcher = (url: string) =>
-    getRequest(url).then((json) => dbEventsToEvents(json));
+    const fetcher = (url: string) =>
+        getRequest(url).then((json) => dbEventsToEvents(json))
 
-  const { data = initialValues || [], mutate } = useSWR<Event[]>(
-    "/api/event",
-    fetcher
-  );
+    const { data = initialValues || [], mutate } = useSWR<Event[]>(
+        '/api/event',
+        fetcher
+    )
 
-  return {
-    events: data,
-    mutateEvents: mutate,
-  };
+    return {
+        events: data,
+        mutateEvents: mutate,
+    }
 }
